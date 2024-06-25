e3aaa40176e1d0cb6ccedd952594c109cb5e4388
kien-ship-it <122910950+kien-ship-it@users.noreply.github.com>
2024-06-25 20:38:22 +0700

    AI api code

--- lib/common/ConstantModel.dart
+++ lib/common/ConstantModel.dart
@@ -1,3 +1,3 @@
 const String LIST_PERSONAL_LESSON = "listPersonalLesson";
 const String LIST_DEFAULT_LESSON = "listDefaultLesson";
-const String API_KEY = "";
+const String API_KEY = "AIzaSyB1WBGxH21Mg-UvoTE3MVhJYwgLzy8XvEs";
