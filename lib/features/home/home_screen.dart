ac66e613a7216c01853bc28162db75822615938b
tvminh19 <tvminh.it@gmail.com>
2024-07-16 21:47:12 +0700

    add list lesson to home screen

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../GlobalData.dart';
+import '../../Widgets/MinimalLessonWidget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -24,37 +25,19 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
+
+    List<Widget> listWidget = [];
+    for (var e in listLesson) {
+      listWidget.add(
+        // UI need to be decorated
+        MinimalLessonWidget(
+          lessonModel: e,
+        ),
+      );
+    }
     return SafeArea(
       child: ListView(
-        children: [
-          Container(
-            margin: const EdgeInsets.fromLTRB(16.0, 20.0, 16.0, 0),
-            // Added margin
-            padding: const EdgeInsets.all(8.0),
-            // Internal padding
-            child: const Text(
-              "Home",
-              textAlign: TextAlign.left, // Align text to the left
-              style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
-            ),
-          ),
-          const SizedBox(height: 8.0),
-          SizedBox(
-            height: height * 5 / 6,
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                // decorate
-                return Container(
-                  margin: const EdgeInsets.all(16.0),
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(listLesson[index].title),
-                );
-              },
-              itemCount: listLesson.length,
-            ),
-          )
-        ],
-      ),
-    );
+        children: [Text("HOME"), ...listWidget],
+    ));
   }
 }
